cdf1b7443630cb7f1e9213cfa3868c735edeb45c
Tomas Holas <tomash@salsitasoft.com>
2014-12-22 15:46:49 +0100

    initial

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,79 @@
+'use strict';
+/*jshint esnext: true */
+
+export default class MainCtrl {
+  constructor () {
+    var vm = this;
+    vm.awesomeThings = [
+      {
+        'title': 'AngularJS',
+        'url': 'https://angularjs.org/',
+        'description': 'HTML enhanced for web apps!',
+        'logo': 'angular.png'
+      },
+      {
+        'title': 'BrowserSync',
+        'url': 'http://browsersync.io/',
+        'description': 'Time-saving synchronised browser testing.',
+        'logo': 'browsersync.png'
+      },
+      {
+        'title': 'GulpJS',
+        'url': 'http://gulpjs.com/',
+        'description': 'The streaming build system.',
+        'logo': 'gulp.png'
+      },
+      {
+        'title': 'Jasmine',
+        'url': 'http://jasmine.github.io/',
+        'description': 'Behavior-Driven JavaScript.',
+        'logo': 'jasmine.png'
+      },
+      {
+        'title': 'Karma',
+        'url': 'http://karma-runner.github.io/',
+        'description': 'Spectacular Test Runner for JavaScript.',
+        'logo': 'karma.png'
+      },
+      {
+        'title': 'Protractor',
+        'url': 'https://github.com/angular/protractor',
+        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
+        'logo': 'protractor.png'
+      },
+      {
+        'title': 'Zepto',
+        'url': 'http://zeptojs.com/',
+        'description': 'The aerogel-weight jQuery-compatible JavaScript library.',
+        'logo': 'zepto.png'
+      },
+      {
+        'title': 'Bootstrap',
+        'url': 'http://getbootstrap.com/',
+        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
+        'logo': 'bootstrap.png'
+      },
+      {
+        'title': 'Angular UI Bootstrap',
+        'url': 'http://angular-ui.github.io/bootstrap/',
+        'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
+        'logo': 'ui-bootstrap.png'
+      },
+      {
+        'title': 'Less',
+        'url': 'http://lesscss.org/',
+        'description': 'Less extends the CSS language, adding features that allow variables, mixins, functions and many other techniques.',
+        'logo': 'less.png'
+      },
+      {
+        'title': 'ES6 (6to5)',
+        'url': 'https://github.com/6to5/6to5',
+        'description': 'Turns ES6+ code into vanilla ES5, so you can use next generation features today.',
+        'logo': '6to5.png'
+      }
+    ];
+    vm.awesomeThings.forEach((awesomeThing) => {
+      awesomeThing.rank = Math.random();
+    });
+  }
+}
