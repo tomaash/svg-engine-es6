f17fcc2ade597aae7c4fb5e5cc37ad73f6858329
Tomas Holas <tomash@salsitasoft.com>
2015-01-29 19:03:20 +0100

    fix server and index problems

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-require('harp').server(__dirname + '/dist', { port: process.env.PORT || 5000 })
+// require('harp').server(__dirname + '/dist', { port: process.env.PORT || 5000 })
+
+var express = require('express');
+var app = express();
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/', express.static(__dirname + '/dist'));
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log('Express (' + app.get('env') + ') server listening on port ' + port);
+});
