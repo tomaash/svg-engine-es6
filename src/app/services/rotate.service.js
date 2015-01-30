d33a2386b6ed4c1bf09acdcac68a9e6b2bb60a7e
Tomas Holas <tomash@salsitasoft.com>
2015-01-30 10:02:41 +0100

    Rotation now more intuitive

--- src/app/services/rotate.service.js
+++ src/app/services/rotate.service.js
@@ -8,8 +8,19 @@ default class Rotate {
     this.currentShape = shape;
     this.oldShape = angular.copy(shape);
     this.initMidpoint = new Victor(this.currentShape.width / 2, this.currentShape.height / 2);
+    
     this.initAngle = this.initMidpoint.angleDeg();
     this.oldDelta = this._computeDelta(this.oldShape.angle);
+    // this.initMidpoint.add(this.oldDelta);
+
+    // Compensate for midpoint delta from old rotation
+    this.oldShape.x += this.oldDelta.x;
+    this.oldShape.y += this.oldDelta.y;
+
+    // Compute absolute midpoint for angle calculation
+    var initPosVector = new Victor(this.currentShape.x, this.currentShape.y);
+    this.currentShape.initAbsMidpoint = initPosVector.add(this.initMidpoint);
+    this.currentShape.initAbsMidpoint.add(this.oldDelta);
   }
 
   _computeMidpoint(angle) {
@@ -30,7 +41,12 @@ default class Rotate {
 
   mouseMove() {
     // Compute new angle
-    this.currentShape.angle = this.oldShape.angle - this.Mouse.state.deltaX;
+    var absMouse = new Victor(this.Mouse.state.currentX, this.Mouse.state.currentY);
+    var rotDirection = absMouse.subtract(this.currentShape.initAbsMidpoint);
+    this.currentShape.angle = rotDirection.angleDeg() - 90;
+
+    // Previous way of angle computation - not so intuitive
+    // this.currentShape.angle =  this.oldShape.angle - this.Mouse.state.deltaX + this.Mouse.state.deltaY;
     
     // Compute new midpoint
     var mp = this._computeMidpoint(this.currentShape.angle);
@@ -41,8 +57,8 @@ default class Rotate {
     var delta = this._computeDeltaFromMidpoint(mp);
 
     // Compensate for delta and also oldDelta from initial rotation != 0 deg
-    this.currentShape.x = this.oldShape.x - delta.x + this.oldDelta.x;
-    this.currentShape.y = this.oldShape.y - delta.y + this.oldDelta.y;
+    this.currentShape.x = this.oldShape.x - delta.x;// + this.oldDelta.x;
+    this.currentShape.y = this.oldShape.y - delta.y;// + this.oldDelta.y;
   }
 
   finish() {
